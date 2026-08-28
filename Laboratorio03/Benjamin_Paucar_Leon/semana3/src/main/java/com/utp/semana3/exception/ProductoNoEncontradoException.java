d824f459542e08fb0335dc57a667bdcf553ec49b
Benjamin <benjaleon610@gmail.com>
2026-08-28 01:40:45 +0000

    Laboratorio03: se retira el scaffold previo, se hara paso a paso desde Spring Initializr

--- Laboratorio03/Benjamin_Paucar_Leon/semana3/src/main/java/com/utp/semana3/exception/ProductoNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.utp.semana3.exception;
-
-public class ProductoNoEncontradoException extends RuntimeException {
-
-    public ProductoNoEncontradoException(Long id) {
-        super("No se encontró el producto con id " + id);
-    }
-}
